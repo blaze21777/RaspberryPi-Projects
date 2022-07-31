5dda7d9e0388bdd77e5bcf80e345fb023b1d1874
blaze21777 <stephen.411@hotmail.co.uk>
2022-07-31 18:13:24 +0100

    Fixed duplicate import

--- DHT11_Humidity_Sensor/record_temp_data.py
+++ DHT11_Humidity_Sensor/record_temp_data.py
@@ -2,16 +2,16 @@
 import sys
 import Adafruit_DHT
 import datetime
-import datetime
 import time
 import csv
 import os
 
 FILENAME = 'temperature_data.csv'
-TIMER =  60 * 15  # Get time in seconds only change second number
+TIMER = 60 * 15  # Get time in seconds only change second number
 startlog = time.time()
 
-temp_headers = ['Date','Year', 'Month', 'Day', 'Time', 'Temperature', 'Humidity']
+temp_headers = ['Date', 'Year', 'Month',
+                'Day', 'Time', 'Temperature', 'Humidity']
 
 # Check if file exists
 if not os.path.exists(FILENAME):
@@ -24,28 +24,28 @@ if not os.path.exists(FILENAME):
 print('press Ctrl + C to end program')
 while True:
     # Only run after set time
-    if time.time() - TIMER > startlog: 
-	print('It is time, saving data')
-	temp_data = []
+    if time.time() - TIMER > startlog:
+        # print('It is time, saving data')
+        temp_data = []
 
-   	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-   	full_datetime = datetime.datetime.now()
-   	year = full_datetime.year
-   	month = full_datetime.month
-   	day = full_datetime.day
-	curr_time = time.strftime("%H:%M:%S", time.localtime())
+        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+        full_datetime = datetime.datetime.now()
+        year = full_datetime.year
+        month = full_datetime.month
+        day = full_datetime.day
+        curr_time = time.strftime("%H:%M:%S", time.localtime())
 
-    	temp_data.append([full_datetime,
-                      year,
-                      month,
-                      day,
-                      curr_time,
-		      temperature,
-                      humidity])
+        temp_data.append([full_datetime,
+                          year,
+                          month,
+                          day,
+                          curr_time,
+                          temperature,
+                          humidity])
 
-    	with open(FILENAME, 'a') as csvfile:
-        	# creating a csv writer object
-        	csvwriter = csv.writer(csvfile)
-       		# writing the data rows
-        	csvwriter.writerows(temp_data)
+        with open(FILENAME, 'a') as csvfile:
+            # creating a csv writer object
+            csvwriter = csv.writer(csvfile)
+            # writing the data rows
+            csvwriter.writerows(temp_data)
         startlog = time.time()
